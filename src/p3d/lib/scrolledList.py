f405e491c4b76fb45dce77d11a30ab7e2ec96d96
Nathan Ogden <me@chompy.co>
2012-05-03 11:28:01 -0400

    - Scrolled list more or less completed.

--- src/p3d/lib/scrolledList.py
+++ src/p3d/lib/scrolledList.py
@@ -28,6 +28,42 @@ class scrolledList(DirectObject):
     sLBG = self.node.attachNewNode(menu_frame.generate())
     sLBG.setTransparency(TransparencyAttrib.MAlpha)
 
+    # Load Scroll Arrow Image
+    self.scrollArrow = loader.loadTexture(base.assetPath + "/menu/scroll_arrow.png")
+
+    # Scroll Buttons
+    self.scrollUp = DirectButton( 
+             scale=.04,
+             frameColor=(.6,.6,.6,0), 
+             pos=(1.245,0,-.06),
+             image=self.scrollArrow,
+             parent=self.node,            
+             #frameTexture=self.arrowTex,
+             relief=DGG.FLAT,
+             rolloverSound=None,
+             clickSound=None,
+             command=self.scrollList,
+             extraArgs=[-1]             
+             ) 
+    self.scrollUp.setTransparency(TransparencyAttrib.MAlpha)    
+    self.scrollUp.hide()
+
+    self.scrollDown = DirectButton( 
+             scale=.04,
+             frameColor=(.6,.6,.6,0), 
+             pos=(1.245,0,-1.445),
+             image=self.scrollArrow,
+             parent=self.node,            
+             #frameTexture=self.arrowTex,
+             relief=DGG.FLAT,
+             rolloverSound=None,
+             clickSound=None,
+             hpr=(180,0,180),
+             command=self.scrollList,
+             extraArgs=[1]
+             ) 
+    self.scrollDown.setTransparency(TransparencyAttrib.MAlpha)  
+
     # Items Node
     self.itemsNode = NodePath("ScrolledList_Items")
     self.itemsNode.reparentTo(self.node)
@@ -55,12 +91,34 @@ class scrolledList(DirectObject):
     self.itemsNode.reparentTo(self.node)
     self.items = []
     self.scroll = 0
+    self.selected = 0
 
     # Add items
     for i in range(len(items)):
-      itemText = OnscreenText(text = items[i], pos = (.076, -.14 + (i* -.1)), scale = 0.07, align=TextNode.ALeft, fg=(1,1,1,1), parent=self.itemsNode, font=self.regFont)
-      if i > 13: itemText.hide()
-      self.items.append(itemText)
+
+      itemBtn = DirectButton( 
+               text=items[i], 
+               text_fg=(1, 1, 1, 1), frameColor=(.6,.6,.6,0), 
+               scale=.07, pos=(.076,0, -.12 + (i * -.1)), 
+               parent=self.itemsNode,
+               text_font=self.regFont,
+               text_align=TextNode.ALeft,
+               #frameTexture=self.arrowTex,
+               frameSize=(-.25, 16.0, -.4, 1.0),
+               relief=DGG.FLAT,
+               rolloverSound=None,
+               clickSound=None,
+               command=self.selectItem,
+               extraArgs=[i],            
+               ) 
+
+      #itemBtn.bind(DGG.WITHIN, self.itemHover, [itemBtn])
+      #itemBtn.bind(DGG.WITHOUT, self.itemUnhover, [itemBtn])      
+      if i > 13: itemBtn.hide()
+      self.items.append(itemBtn)
+
+    # Hide Scroll Down Button if less then 14 items
+    if len(self.items) < 14: self.scrollDown.hide()
 
   def scrollList(self, amt):
 
@@ -80,12 +138,52 @@ class scrolledList(DirectObject):
       else:
         self.items[i].hide()
 
+    # Hide/Show Scroll Arrows
+    if self.scroll >= len(self.items) - 14: self.scrollDown.hide()
+    else: self.scrollDown.show()
+
+    if self.scroll <= 0: self.scrollUp.hide()
+    else: self.scrollUp.show()
+
+  def selectItem(self, number):
+
+    """
+    Make item selection.
+    """
+
+    self.items[self.selected]['frameColor'] = (.6,.6,.6,0)
+
+    self.selected = number
+    self.items[self.selected]['frameColor'] = (.6,.6,.6,.4)
+
+    # Adjust scroll if needed.
+    if self.selected - self.scroll < 0:
+      self.scrollList(self.selected - self.scroll)
+
+    if self.selected - self.scroll > 13:
+      self.scrollList(abs(13 - (self.selected - self.scroll)))
+
+  def changeSelection(self, inc):
+
+    """
+    Change item selection with an increment.
+    """
+
+    newVal = self.selected + inc
+    if newVal > len(self.items) - 1: newVal = 0
+    if newVal < 0: newVal = len(self.items) - 1
+
+    self.selectItem(newVal)
+
   def acceptInput(self):
 
     """
     Setup input to control scrolled list.
     """
 
-    self.accept("wheel_up", self.scrollList, [-1])
-    self.accept("wheel_down", self.scrollList, [1])  
-    
+    base.accept("wheel_up", self.scrollList, [-1])
+    base.accept("wheel_down", self.scrollList, [1])  
+
+    for x in range(4):
+      base.accept("p" + str(x + 1) + "_up", self.changeSelection, [-1])
+      base.accept("p" + str(x + 1) + "_down", self.changeSelection, [1]) 
